6759cca212e97803a76ff2410281693444819de1
Eugeny Sobolenko <stereotype1@mail.ru>
2019-07-30 22:07:24 +0400

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,42 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Eugeny',
+        username: 'preql',
+        avatar_url: 'http://pikchyriki.net/avatar/krutye/100/74.jpg'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет, Женёк!"
+    @user = User.new(
+      name: 'Eugeny',
+      username: 'preql',
+      avatar_url: 'http://pikchyriki.net/avatar/krutye/100/74.jpg'
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(
+        text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
+      ),
+      Question.new(
+        text: 'Кто по жизни?', created_at: Date.parse('27.03.2016')
+      ),
+      Question.new(
+        text: 'За Митю Гладкого слыхал?', created_at: Date.parse('27.03.2016')
+      )
+    ]
+
+    @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
+
+  def sklonenie(number, krokodil, krokodila, krokodilov)
+
+    if number == nil || !number.is_a?(Numeric)
+      number = 0
+    end
+
+    if (number % 100 == 11) || (number % 100 == 12) ||
+      (number % 100 == 13)  || (number % 100 == 14)
+      return krokodilov
+    end
+
+    ostatok = number % 10
+
+    if (ostatok == 1)
+      return krokodil
+    end
+
+    if (ostatok >= 2 && ostatok <= 4)
+      return krokodila
+    end
+
+    if (ostatok > 4 || ostatok == 0)
+      return krokodilov
+    end
+  end
 end
