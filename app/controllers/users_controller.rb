2e8661d4c44cc5f0fbd2f85d493479baaf0921ba
Eugeny Sobolenko <stereotype1@mail.ru>
2019-07-31 21:11:13 +0400

    Views reafactoring + Add block statistics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,12 +30,16 @@ class UsersController < ApplicationController
         text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
       ),
       Question.new(
-        text: 'Кто по жизни?', created_at: Date.parse('27.03.2016')
+        text: 'Кто по жизни?', created_at: Date.parse('27.03.2016'), answer: 'Волк'
       ),
       Question.new(
-        text: 'За Митю Гладкого слыхал?', created_at: Date.parse('27.03.2016')
-      )
+        text: 'За Митю Гладкого слыхал?', created_at: Date.parse('27.03.2016'))
     ]
+
+    @questions_count = @questions.count
+    @answered_count =
+      @questions.select { |question| question.answer != nil }.count
+    @unanswered_count = @questions_count - @answered_count
 
     @new_question = Question.new
   end
