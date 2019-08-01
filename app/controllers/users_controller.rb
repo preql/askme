62a37fc9c17856ac789021af73c54a6edb5f01ae
Eugeny Sobolenko <stereotype1@mail.ru>
2019-08-01 18:46:39 +0400

    count for questions, answers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,21 +24,9 @@ class UsersController < ApplicationController
       avatar_url: 'http://pikchyriki.net/avatar/krutye/100/74.jpg'
     )
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(
-        text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
-      ),
-      Question.new(
-        text: 'Кто по жизни?', created_at: Date.parse('27.03.2016'), answer: 'Волк'
-      ),
-      Question.new(
-        text: 'За Митю Гладкого слыхал?', created_at: Date.parse('27.03.2016'))
-    ]
-
+    @questions = Question.all
     @questions_count = @questions.count
-    @answered_count =
-      @questions.select { |question| question.answer != nil }.count
+    @answered_count = @questions.where.not(answer: nil).count
     @unanswered_count = @questions_count - @answered_count
 
     @new_question = Question.new
