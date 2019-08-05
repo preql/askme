831669d14168314f7c7679ba5c58b506c6a8be91
Eugeny Sobolenko <stereotype1@mail.ru>
2019-08-05 21:44:46 +0400

    add column author to question, add color_field for user, fix bugs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,6 +10,8 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
+    @question.author_id = current_user.id if current_user.present?
+
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ApplicationRecord
   validates :email, :username, uniqueness: true
   validates :username, length: { maximum: 40 },
             format: { with: /\A[a-zA-Z0-9\_]+\z/ }
-  validates :color, format: { with: /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/ }
+  # validates :color, format: { with: /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/ },
+  #           on: :edit
 
   attr_accessor :password
 
