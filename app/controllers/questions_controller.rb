3ef077d65b5cca1406cbdfc2e25440e37cc5d7af
Eugeny Sobolenko <stereotype1@mail.ru>
2019-08-07 19:28:05 +0400

    add question author optional

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
-    @question.author_id = current_user.id if current_user.present?
+    @question.author = current_user
 
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
